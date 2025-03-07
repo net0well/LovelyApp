fb1d0dfa45f46d01e899a60c6906f8fbe1b4e0f9
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-07 20:34:44 -0300

    creating 2 endpoints

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users =  await context.Users.ToListAsync();
+
+        return Ok(users);
+    }
+    
+    [HttpGet("{userId:int}")]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUser(int userId)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+}
