ef3e9c0e5623ce102f703c2df0428ed42240201a
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-07 22:43:15 -0300

    adding a cors policy support on api and making first http request in angular

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,14 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+
+  http = inject(HttpClient);
+  title = 'Lovely';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7029/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log("Request completed")
+    })
+  }
 }
