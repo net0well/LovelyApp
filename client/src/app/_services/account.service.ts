02115dc7f87c2e3fdc24e6eafe30d9e852d7176f
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-09 22:48:25 -0300

    child to parent communication

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { User } from '../_models/user';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,22 @@ export class AccountService {
 
   private http = inject(HttpClient)
   baseurl = 'https://localhost:5001/api/';
+  currentUser = signal<User | null>(null);
+
+
   Login(model: any) {
-    return this.http.post(this.baseurl+'account/login', model);
+    return this.http.post<User>(this.baseurl+'account/login', model).pipe(
+      map(user => {
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    )
+  }
+
+  Logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
   }
 }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import {Component, input, output} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  userFromHomeComponent = input.required<any>();
+  cancelRegister = output<boolean>();
+
+  model: any = {};
+
+  register() {
+    console.log(this.model)
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+}
