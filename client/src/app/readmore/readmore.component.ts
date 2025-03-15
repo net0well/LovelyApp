b5ed261e258aba99b6fcc250ce88408b65d6edee
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-15 04:27:24 -0300

    fix

--- client/src/app/readmore/readmore.component.ts
+++ client/src/app/readmore/readmore.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { Component, inject } from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-readmore',
