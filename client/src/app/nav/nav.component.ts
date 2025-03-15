36a530441c396520bc87928c29a63bda86e2536f
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-15 02:53:12 -0300

    routing in code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,7 +3,7 @@ import { FormsModule } from "@angular/forms";
 
 import { AccountService } from '../_services/account.service';
 import {BsDropdownModule} from "ngx-bootstrap/dropdown";
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -14,12 +14,13 @@ import {RouterLink, RouterLinkActive} from "@angular/router";
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
   model: any = {};
 
   Login(){
     this.accountService.Login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: () => {
+        this.router.navigateByUrl('/members');
       },
       error: error => {
         console.log(error);
@@ -29,5 +30,6 @@ export class NavComponent {
 
   Logout() {
     this.accountService.Logout();
+    this.router.navigateByUrl('/');
   }
 }
