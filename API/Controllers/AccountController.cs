e13ffb32cea47e96a354b827163c780acddabd6f
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-16 01:26:37 -0300

    extending user entity

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -15,8 +15,9 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-        
-        using var hmac = new HMACSHA512();
+
+        return Ok();
+        /*using var hmac = new HMACSHA512();
 
         var user = new AppUser()
         {
@@ -32,7 +33,7 @@ public class AccountController(DataContext context, ITokenService tokenService) 
        {
            Username = user.UserName,
            Token = tokenService.CreateToken(user)
-       };
+       };*/
     }
 
     [HttpPost("login")]
