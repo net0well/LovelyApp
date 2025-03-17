a1efc1da35e49f5a2ac6321b24cdf74c95ceba07
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-16 22:08:53 -0300

    using automapper queryable extensions

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,8 +19,8 @@ public class AppUser
     public required string Country { get; set; }
     public List<Photo> Photos { get; set; } = [];
 
-    public int GetAge()
+    /*public int GetAge()
     {
         return DateOfBirth.CalculateAge();
-    }
+    }*/
 }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using AutoMapper;
 
 namespace API.Helpers;
@@ -9,11 +10,16 @@ public class AutoMapperProfiles : Profile
     public AutoMapperProfiles()
     {
         CreateMap<AppUser, MemberDto>()
+            .ForMember(d =>
+                d.Age, o =>
+                     o.MapFrom(s =>
+                            s.DateOfBirth.CalculateAge()))
             .ForMember(d => d.PhotoUrl,
                 o =>
                     o.MapFrom(s =>
                         s.Photos.FirstOrDefault(x =>
                             x.IsMain)!.Url));
+        
         CreateMap<Photo, PhotoDto>();
     }
 }
