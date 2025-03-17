be914d4f8a5fc71b7f8b675cc3729c32d686ff0d
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-17 01:06:07 -0300

    creating the members component and improving the layout

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import {Component, inject, OnInit} from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
-import {HttpClient} from "@angular/common/http";
 import {RouterLink} from "@angular/router";
 
 @Component({
@@ -10,14 +9,9 @@ import {RouterLink} from "@angular/router";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent {
   registerMode = false;
-  users: any;
-  http = inject(HttpClient);
 
-  ngOnInit() {
-    this.getUsers()
-  }
 
   registerToggle(){
     this.registerMode = !this.registerMode;
@@ -27,12 +21,5 @@ export class HomeComponent implements OnInit{
     this.registerMode = event;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("Request completed")
-    })
-  }
 
 }
