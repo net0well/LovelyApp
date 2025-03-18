f77f2c63fe9b06c40d83c8d0a0a93689f01c6d72
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-17 21:18:49 -0300

    routing to the detailed page

--- client/src/app/members/members-card/members-card.component.ts
+++ client/src/app/members/members-card/members-card.component.ts
@@ -1,10 +1,11 @@
 import {Component, input, ViewEncapsulation} from '@angular/core';
 import {Member} from '../../_models/member';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-members-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './members-card.component.html',
   styleUrl: './members-card.component.css'
 })
