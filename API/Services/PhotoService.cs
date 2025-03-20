62a5b5c4ae34c3eed803d0d2a4bb8162fe94c57c
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-19 23:20:37 -0300

    adding a photo service

---
+++ API/Services/PhotoService.cs
@@ -0,0 +1,52 @@
+using API.Helpers;
+using API.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+
+namespace API.Services;
+
+public class PhotoService : IPhotoService
+{
+    private readonly IOptions<CloudinarySettings> _config;
+    private readonly Cloudinary _cloudinary;
+    
+    public PhotoService(IOptions<CloudinarySettings> config)
+    {
+        _config = config;
+
+        var acc = new Account(_config.Value.CloudName, _config.Value.ApiKey, _config.Value.ApiSecret);
+
+        _cloudinary = new Cloudinary(acc);
+    }
+    
+    public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+    {
+        var uploadResult = new ImageUploadResult();
+
+        if (file.Length > 0)
+        {
+            using var stream = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Transformation = new Transformation()
+                    .Height(500)
+                    .Width(500)
+                    .Crop("fill")
+                    .Gravity("face"),
+                Folder = "lovely"
+            };
+
+            uploadResult = await _cloudinary.UploadAsync(uploadParams);
+        }
+        return uploadResult;
+    }
+
+    public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+    {
+        var deleteParams = new DeletionParams(publicId);
+
+        return await _cloudinary.DestroyAsync(deleteParams);
+    }
+}
