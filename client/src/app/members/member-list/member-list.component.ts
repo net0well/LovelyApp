ad8053d80d9bc5bb3fdc1e9ef68721b1de5d52c9
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-25 22:54:20 -0300

    adding pagination in the client side

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,23 +2,34 @@ import {Component, OnInit, inject} from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { MembersCardComponent } from '../members-card/members-card.component';
+import {PaginationModule} from "ngx-bootstrap/pagination";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MembersCardComponent],
+  imports: [MembersCardComponent, PaginationModule, CommonModule],
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
    memberService = inject(MembersService);
+   pageNumber = 1;
+   pageSize = 12;
 
     ngOnInit(): void {
-      if(this.memberService.members().length === 0){
+      if(!this.memberService.paginatedResult()){
         this.loadMembers();
       }
     }
     loadMembers() {
-      this.memberService.getMembers()
+      this.memberService.getMembers(this.pageNumber, this.pageSize)
+    }
+
+    pageChanged(event: any){
+      if(this.pageNumber != event.page){
+        this.pageNumber = event.page;
+        this.loadMembers();
+      }
     }
 }
