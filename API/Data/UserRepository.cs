ba3acad0240880fa11ff3974b777eb3e3765e541
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-26 21:31:55 -0300

    aditional filter

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -59,7 +59,11 @@ public class UserRepository : IUserRepository
         {
             query = query.Where(x => x.Gender == userParams.Gender);
         }
-            
+
+        var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+        var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+        query = query.Where(x => x.DateOfBirth >= minDob && x.DateOfBirth <= maxDob);
 
         return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider), userParams.PageNumber, userParams.PageSize);
 
--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -14,4 +14,8 @@ public class UserParams
 
     public string? Gender { get; set; }
     public string? CurrentUsername { get; set; }
+
+    public int MinAge { get; set; } = 18;
+
+    public int MaxAge { get; set; } = 100;
 }
