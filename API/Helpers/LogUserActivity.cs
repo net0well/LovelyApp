20ba02f2b6faf959411631086a96d6da47965c41
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-27 23:32:07 -0300

    making the last activity action filter more optimal

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -14,10 +14,10 @@ public class LogUserActivity : IAsyncActionFilter
 
         if (context.HttpContext.User.Identity?.IsAuthenticated != true) return;
 
-        var userName = resultContext.HttpContext.User.GetUsername();
+        var userId = resultContext.HttpContext.User.GetUserId();
 
         var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
-        var user = await repo.GetUserByUsernameAsync(userName);
+        var user = await repo.GetUserByIdAsync(userId);
 
         if (user == null) return;
         
