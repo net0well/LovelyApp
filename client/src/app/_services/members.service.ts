e43f886a09c311baa88ff7d9b239f234e67fad20
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-30 21:45:25 -0300

    restoring caching for member detailed

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -60,6 +60,13 @@ export class MembersService {
     /*const member = this.members().find(x => x.userName === username);
     if(member !== undefined) return of(member);*/
 
+    const member: Member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.body), [])
+      .find((m: Member) => m.userName == username);
+
+    if(member) return of(member);
+
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
