0600e16b8f544e3f8c29258dd192fb79bcadd5c2
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-03-30 21:59:22 -0300

    remembering  the filters for a user in the service

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -16,9 +16,7 @@ import {ButtonsModule} from "ngx-bootstrap/buttons";
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit{
-   private accountService = inject(AccountService)
    memberService = inject(MembersService);
-   userParams = new UserParams(this.accountService.currentUser());
    genderList = [{value: 'male', display: 'Homens'}, {value: 'female', display: 'Mulheres'}]
 
     ngOnInit(): void {
@@ -27,24 +25,24 @@ export class MemberListComponent implements OnInit{
       }
     }
     loadMembers() {
-      this.memberService.getMembers(this.userParams)
+      this.memberService.getMembers()
     }
 
     resetFilters(){
-     this.userParams = new UserParams(this.accountService.currentUser());
+     this.memberService.resetUserParams();
      this.loadMembers();
     }
 
   validateMinAge() {
-    if (this.userParams.minAge < 18) {
-      this.userParams.minAge = 18;
+    if (this.memberService.userParams().minAge < 18) {
+      this.memberService.userParams().minAge = 18;
     }
   }
 
 
     pageChanged(event: any){
-      if(this.userParams.pageNumber != event.page){
-        this.userParams.pageNumber = event.page;
+      if(this.memberService.userParams().pageNumber != event.page){
+        this.memberService.userParams().pageNumber = event.page;
         this.loadMembers();
       }
     }
