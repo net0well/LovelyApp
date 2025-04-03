93e030d46a9bb3634225f9628c6b9242f019b9fd
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-04-02 23:09:15 -0300

    adding the list component and likes services

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import { LikesService } from '../_services/likes.service';
+import { Member } from '../_models/member';
+import {ButtonsModule} from "ngx-bootstrap/buttons";
+import {FormsModule} from "@angular/forms";
+import { MembersCardComponent } from '../members/members-card/members-card.component';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, MembersCardComponent, NgClass],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit{
+  private likesService = inject(LikesService);
+  members: Member[] = [];
+  predicate = 'liked';
 
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  getTitle(){
+    switch (this.predicate){
+      case 'liked': return 'Pessoas que você curtiu';
+      case 'likedBy': return 'Pessoas que curtiram você';
+      default: return 'Deram match'
+    }
+  }
+  loadLikes() {
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => this.members = members
+    })
+  }
 }
