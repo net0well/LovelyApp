35753086ae9f4a303f67f767c5c7dba84754a247
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-07-14 22:44:29 -0300

    restoring the project and add message controller

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ba02f2b6faf959411631086a96d6da47965c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8230737f3e5d97390f523c19e752b16eba1c090")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
