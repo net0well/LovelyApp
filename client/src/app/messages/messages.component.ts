c4b342f606b6e55e01c1019b66bb78cca9aea349
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-07-17 00:33:46 -0300

    designing the inbox

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,10 +1,21 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {MessageService} from "../_services/message.service";
+import {ButtonsModule} from "ngx-bootstrap/buttons";
+import {FormsModule} from "@angular/forms";
+import { TimeagoModule, TimeagoFormatter, TimeagoDefaultFormatter, TimeagoClock, TimeagoIntl, TimeagoDefaultClock } from 'ngx-timeago';
+import {Message} from "../_models/message";
+import {RouterLink} from "@angular/router";
+import {PaginationModule} from "ngx-bootstrap/pagination";
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterLink, PaginationModule],
+  providers: [
+    { provide: TimeagoFormatter, useClass: TimeagoDefaultFormatter },
+    { provide: TimeagoClock, useClass: TimeagoDefaultClock },
+    TimeagoIntl
+  ],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
@@ -20,6 +31,11 @@ export class MessagesComponent implements OnInit {
 
   loadMessages(){
     this.messageService.getMessages(this.pageNumber, this.pageSize, this.container);
+  }
+
+  getRoute(message: Message){
+    if(this.container === 'Outbox') return `/members/${message.recipientUsername}`;
+    else return `/members/${message.senderUsername}`;
   }
 
   pageChanged(event: any){
