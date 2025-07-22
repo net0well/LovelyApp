cb71c83a7e1a563983bead4501e7f6a2d856740d
WELLINGTON  NETO <116044216+net0well@users.noreply.github.com>
2025-07-21 23:53:37 -0300

    adding route resolver

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35753086ae9f4a303f67f767c5c7dba84754a247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1493bda56dd69614fd6e3cd3d01af2a1979a82d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import { MembersService } from '../../_services/members.service';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import { Member } from '../../_models/member';
 import {TabDirective, TabsetComponent, TabsModule} from 'ngx-bootstrap/tabs';
 import {GalleryItem, GalleryModule, ImageItem} from 'ng-gallery';
@@ -16,60 +16,108 @@ import {Message} from "../../_models/message";
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent implements OnInit{
+export class MemberDetailComponent implements OnInit {
   @ViewChild('memberTabs') memberTabs?: TabsetComponent;
+
   private messageService = inject(MessageService);
   private memberService = inject(MembersService);
   private route = inject(ActivatedRoute);
-  member?: Member;
+  private router = inject(Router);
+
+  member: Member = {} as Member;
   images: GalleryItem[] = [];
   activeTab?: TabDirective;
   messages: Message[] = [];
 
+  activeTabHeading: string = '';
+
   ngOnInit(): void {
-      this.loadMember()
-
-      this.route.queryParams.subscribe({
-        next: params => {
-          params['tab'] && this.selectTab(params['tab'])
+    this.route.data.subscribe({
+      next: data => {
+        this.member = data['member'];
+        if (this.member && this.member.photos) {
+          this.images = this.member.photos.map(p =>
+            new ImageItem({ src: p.url, thumb: p.url })
+          );
         }
-      })
-  }
 
+        if (!this.activeTabHeading) {
+          this.activeTabHeading = 'Sobre ' + this.member.knownAs;
+        }
+      }
+    });
+
+    this.route.queryParams.subscribe({
+      next: params => {
+        if (params['tab']) {
+          this.activeTabHeading = params['tab'];
+          if (params['tab'] === 'Mensagens') {
+            this.loadMessages();
+          }
+        }
+      }
+    });
+  }
 
   onTabActivated(data: TabDirective): void {
     this.activeTab = data;
-    if(this.activeTab.heading == 'Mensagens' && this.messages.length === 0 && this.member) {
+    this.activeTabHeading = data.heading || '';
+
+    if (data.heading && this.member) {
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { tab: data.heading },
+        queryParamsHandling: 'merge'
+      });
+    }
+
+    if (data.heading === 'Mensagens') {
+      this.loadMessages();
+    }
+  }
+
+  private loadMessages(): void {
+    if (this.messages.length === 0 && this.member && this.member.userName) {
+
       this.messageService.getMessageThread(this.member.userName).subscribe({
-        next: messages => this.messages = messages
-      })
-
-      console.log(this.member.userName);
+        next: messages => {
+          this.messages = messages;
+          console.log('Messages loaded:', messages.length);
+        },
+        error: error => {
+          console.error('Error loading messages:', error);
+        }
+      });
     }
   }
 
-  selectTab(heading: string){
-    if(this.memberTabs){
-      const messageTab = this.memberTabs.tabs.find(x => x.heading === heading);
-      if(messageTab){
-        messageTab.active = true;
+  isTabActive(tabHeading: string): boolean {
+    return this.activeTabHeading === tabHeading;
+  }
+
+  selectTab(tabsetComponent: TabsetComponent, heading: string): void {
+    console.log('Selecting tab:', heading);
+
+    if (tabsetComponent && tabsetComponent.tabs) {
+      const targetTab = tabsetComponent.tabs.find(x => x.heading === heading);
+      if (targetTab) {
+        targetTab.active = true;
+        this.activeTabHeading = heading;
+
+        this.router.navigate([], {
+          relativeTo: this.route,
+          queryParams: { tab: heading },
+          queryParamsHandling: 'merge'
+        });
+
+        if (heading === 'Mensagens') {
+          this.loadMessages();
+        }
+      } else {
+        console.error('Tab not found:', heading);
       }
+    } else {
+      console.error('Tabset component not available');
     }
-  }
-
-  loadMember() {
-  const username = this.route.snapshot.paramMap.get('username');
-  if(!username) return;
-  this.memberService.getMember(username).subscribe({
-    next: member => {
-      this.member = member;
-      member.photos.map(p => {
-        this.images.push(new ImageItem({
-          src: p.url,
-          thumb: p.url
-        }))
-      })
-    }
-  })
   }
 }
